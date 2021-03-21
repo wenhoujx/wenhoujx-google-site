f43e133807a03023cbf9d91d00826350683118e5
Wenshuai Hou <whou@palantir.com>
2021-03-21 16:37:11 -0400

    12123131

--- src/App.js
+++ src/App.js
@@ -2,66 +2,121 @@ import './App.css';
 import { produce } from 'immer';
 import { useState } from 'react'
 import Square from './components/Square';
-import { nRows, nCols } from './components/Game'
-
 
 function App() {
-  const [board, setBoard] = useState(() => {
-    const rows = []
-    for (let i = 0; i < nRows; i++) {
-      rows.push(Array.from(Array(nCols), () => 0))
-    }
-    return rows
-  })
-  const [turn, setTurn] = useState(-1)
 
-  // click a square 
-  const onSelect = (row, col) => {
-    if (board[row][col] === 0) {
-      setBoard((currentBoard) => produce(currentBoard, copy => { copy[row][col] = turn }))
-      nextTurn()
-    }
+  const nRows = 30;
+  const nCols = 30;
+  const maxHistory = 10;
+  const nWins = 5;
+
+  const directions = [
+    [0, 1], [0, -1], [1, 1], [1, -1], [1, 0], [-1, 0], [-1, 1], [-1, -1]
+  ]
+  const initialState = {
+    // 0 is the most recent past state 
+    past: [],
+    current: (() => {
+      return ({
+        board: Array(nRows).fill().map((row) => Array(nCols).fill(0)),
+        turn: -1,
+        winningColor: 0
+      })
+    })()
   }
 
-  const nextTurn = () => {
-    setTurn(turn === -1 ? 1 : -1)
-  }
+  const [state, setState] = useState(() => initialState)
 
-  const clearBoard = () => {
-    setBoard((currentBoard) => {
-      return produce(currentBoard, copy => {
-        for (let i = 0; i < nRows; i++) {
-          for (let j = 0; j < nCols; j++) {
-            copy[i][j] = 0
-          }
+  const placePiece = (row, col) => {
+    console.log('set piece ' + row + ' ' + col)
+    if (state.current.winningColor !== 0) {
+      return;
+    }
+    setState((s) => {
+      return produce(s, copy => {
+        if (copy.current.board[row][col] === 0) {
+          copy.past = [copy.current, ...copy.past.slice(0, maxHistory - 1)]
+          copy.current = produce(copy.current, (cur) => {
+            cur.board[row][col] = cur.turn
+            cur.turn = - cur.turn
+            cur.winningColor = winningColor(cur.board)
+          })
         }
       })
     })
-    // always start with black
-    setTurn(-1)
   }
 
-  const regretOnce = () => {
-
+  const winningColor = (board) => {
+    for (let i = 0; i < nRows; i++) {
+      for (let j = 0; j < nCols; j++) {
+        const startingColor = board[i][j]
+        if (startingColor === 0) {
+          continue
+        }
+        for (const [x, y] of directions) {
+          let line = 1;
+          // start from 1 b/c the starting piece counts as 1
+          for (let step = 1; step < nWins; step++) {
+            const newI = i + x * step
+            const newJ = j + y * step
+            if (newI >= 0 && newI < nRows && newJ >= 0 && newJ < nCols) {
+              if (board[newI][newJ] === startingColor) {
+                line += 1
+              } else {
+                break
+              }
+            }
+          }
+          if (line === nWins) {
+            return startingColor
+          }
+        }
+      }
+    }
+    return 0
   }
+
+  const clearBoard = () => {
+    setState(initialState)
+  }
+
+  const rollBack = () => {
+    setState((s) => {
+      return produce(s, copy => {
+        if (copy.past.length !== 0) {
+          console.log('roll back')
+          copy.current = copy.past[0]
+          console.log(JSON.stringify(copy.past[0]))
+          console.log(JSON.stringify(copy.current))
+          copy.past = copy.past.slice(1)
+        }
+      })
+    })
+  }
+
+  const showNext = () => {
+    if (state.current.winningColor !== 0) {
+      return state.current.winningColor === -1 ? 'black win!' : 'white win!'
+    } else {
+      return state.current.turn === -1 ? 'black next' : 'white next'
+    }
+  }
+
   return (
     <div className='container'>
-
-
-
       <div className='board'
         style={{
           display: 'grid',
           gridTemplateColumns: `repeat(${nCols}, 30px)`
         }}
       >
-        {board.map((row, rowIndex) =>
+        {state.current.board.map((row, rowIndex) =>
           row.map((_, colIndex) =>
             <Square
               row={rowIndex}
               col={colIndex}
-              value={board[rowIndex][colIndex]}
-              onSelect={onSelect}
+              value={state.current.board[rowIndex][colIndex]}
+              onSelect={() => placePiece(rowIndex, colIndex)}
             />
           )
         )}
@@ -69,14 +124,14 @@ function App() {
 
       <div className='controls'>
         <div className='turn-shower'>
-          {turn === -1 ? 'black/黑 turn' : 'white/白 turn'}
+          {showNext()}
         </div>
 
         <button
           className='btn'
-          onClick={regretOnce}
+          onClick={rollBack}
         >
-          Regret Once</button>
+          Roll Back</button>
 
         <button
           className='btn'
--- src/components/game.js
+++ src/components/game.js
@@ -1,11 +1,48 @@
+import produce from 'immer';
+import { useState, useRef } from 'react'
+
 export const nRows = 30;
 export const nCols = 30;
 const maxHistory = 3;
 
-const initialState = {
-    past: [],
-    present: (() => {
-        return Array(nRows).fill().map((row) => Array(nCols).fill(0))
-    })(),
-    future: []
+export const placePiece = (row, col) => {
+    setState((s) => {
+        return produce(s, copy => {
+            if (copy.current.board[row, col] !== 0) {
+                copy.past = [copy.current, ...copy.past[0, maxHistory - 1]]
+                copy.current.board[row, col] = s.turn
+                copy.current.turn = -copy.turn
+            }
+        })
+    })
 }
+
+export const clearBoard = () => {
+    setState(initialState)
+}
+
+export const rollBack = () => {
+    setState((s) => {
+        return produce(s, copy => {
+            if (copy.past.length !== 0) {
+                copy.current = copy.past[0]
+                copy.past = copy.past.slice(1)
+            }
+        })
+    })
+}
+
+const initialState = {
+    // 0 is the most recent state 
+    past: [],
+    current: (() => {
+        return ({
+            board: Array(nRows).fill().map((row) => Array(nCols).fill(0)),
+            turn: -1,
+        })
+    })()
+}
+
+const [state, setState] = useState(initialState)
+
+export const currentStateRef = useRef(state.current)
