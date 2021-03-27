76664186cf64896d46db4666b62845e78e5a05d9
Wenshuai Hou <whou@palantir.com>
2021-03-27 12:52:25 -0400

    move route to the same file in App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
 import './App.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, useLocation } from 'react-router-dom'
 import Gomoku from './apps/gomoku/Gomoku'
 import GameOfLife from './apps/game-of-life/GameOfLife';
-import AboutMe from './apps/homepage/AboutMe';
-import Homepage from './apps/homepage/Homepage';
-
+import AboutMe from './apps/about-me/AboutMe';
 
 function App() {
+
+  const homepage = () => {
+    return (
+      <div
+        className='homepage-container'
+      >
+        <header
+          className='header'
+        >
+          <h1>Wenhoujx homepage</h1>
+        </header>
+        <Link to='/gomoku'>Gomoku(Connect-Five) Game</Link>
+        <Link to='/game-of-life'>Conway's Game of Life Simulation</Link>
+        <br />
+
+        <Link to='/me'>About Me</Link>
+      </div>
+    )
+
+  }
+
   return (
-    <Router>
-      <Route
-        path='/'
-        component={Homepage}
-      />
-      <Route
-        path='/me'
-        component={AboutMe}
-      />
-      <Route
-        path='/gomoku'
-        component={Gomoku}
-      />
-      <Route
-        path='/game-of-life'
-        component={GameOfLife}
-      />
-    </Router>
+    <>
+      <Router>
+        <Route
+          exact
+          path='/'
+          render={homepage}
+        />
+        <Route
+          path='/me'
+          component={AboutMe}
+        />
+        <Route
+          path='/gomoku'
+          component={Gomoku}
+        />
+        <Route
+          path='/game-of-life'
+          component={GameOfLife}
+        />
+      </Router>
+    </>
   );
 }
 
--- src/apps/game-of-life/GameOfLife.js
+++ src/apps/game-of-life/GameOfLife.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { produce } from 'immer'
 import './GameOfLife.css'
