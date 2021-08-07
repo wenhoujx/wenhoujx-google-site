a7128259426bd15f66cab0d9eaea69cf7b92e042
Wenshuai Hou <whou@palantir.com>
2021-08-07 17:14:13 -0400

    Update: theme.js

---
+++ src/theme.js
@@ -0,0 +1,25 @@
+export default {
+  fonts: {
+    body: 'Georgia, Cambria, "Times New Roman", Times, serif',
+    heading: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif',
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  colors: {
+    text: '#392a25',
+    muted: '#aaaaaa',
+    background: '#f8f8f8',
+    foreground: '#ffffff',
+    primary: '#92b955',
+    secondary: '#9f8a6d',
+  },
+  space: [0, 4, 8, 16, 32, 48],
+  breakpoints: ['40em', '64em', '80em'],
+};
