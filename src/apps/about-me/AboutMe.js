02fba75c1a32df6e2d62bf662529ca7c16f26564
Wenshuai Hou <whou@palantir.com>
2021-08-07 17:14:13 -0400

    Update: AboutMe.js

--- src/apps/about-me/AboutMe.js
+++ src/apps/about-me/AboutMe.js
@@ -1,29 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Flex, Text } from '@theme-ui/components'
 
 const AboutMe = () => {
     return (
-        <div
-            className='aboutme'
-        >
-            <p>
-                I am a backend dev work mostly in java JVM world.
-            </p>
-            <p>
-                This personal website is my personal journey to explore the frontend and react.
-            </p>
-            <br />
-            <p>
-                <a href='mailto:wenhoujx@gmail.com' subject='From wenhoujx.com'>
-                    Contact Me via Email: wenhoujx@gmail.com
-            </a>
-            </p>
-            <br />
-            <p>
-                <Link to='/'>Go Back to Home</Link>
-            </p>
-
-        </div>
+        <Flex p={2} sx={{
+            flexDirection: "column"
+        }}>
+            <Text>I am a backend dev work mostly in java JVM world.</Text>
+            <Text>This personal website is my personal journey to explore the frontend and react.</Text>
+            <Text><a href='mailto:wenhoujx@gmail.com' subject='From wenhoujx.com'>
+                Contact Me via Email: wenhoujx@gmail.com
+            </a></Text>
+            <Link to='/'>Go Back to Home</Link>
+        </Flex>
     )
 }
 
