6ccf379e96508dc9f79065dc3d7aac9c8533c95a
Wenshuai Hou <whou@palantir.com>
2021-08-15 21:40:44 -0400

    Update: App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,64 @@
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import Gomoku from './apps/gomoku/Gomoku'
-import GameOfLife from './apps/game-of-life/GameOfLife';
-import AboutMe from './apps/about-me/AboutMe';
-import GreWords from './apps/gre-words/GreWords';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import theme from "./theme"
-import { Container, Box, Message } from "theme-ui"
-import { Cookings } from "./apps/cookings/Cookings"
+import { Box, Card, Container, Message, Text, Grid, Image, Flex, Avatar } from "theme-ui";
+import AboutMe from './apps/about-me/AboutMe';
+import { Cookings } from "./apps/cookings/Cookings";
+import GameOfLife from './apps/game-of-life/GameOfLife';
+import Gomoku from './apps/gomoku/Gomoku';
+import GreWords from './apps/gre-words/GreWords';
+import { theme } from "./theme";
+import { chefHatIcon, conwayIcon, gomokuIcon, greIcon, meIcon } from "./icons"
 
 function App() {
   const homepage = () => {
     return (
       <ThemeProvider theme={theme}>
         <Container
-          p={[2, 3]}
-          sx={{
-            display: "flex",
-            flexDirection: "column"
-          }}>
+          px={300} >
           <Box>
-            <h1><Message> Wenhoujx homepage </Message></h1>
+            <h2><Message>Wenhoujx homepage</Message></h2>
           </Box>
-          <Box p={2}>
-            <Link to='/cookings'>My Cooking Recipes</Link>
-          </Box>
-          <Box p={2}>
-            <Link to='/gomoku'>Gomoku(Connect-Five) Game</Link>
-          </Box>
-          <Box p={2}>
-            <Link to='/game-of-life'>Conway's Game of Life Simulation</Link>
-          </Box>
-          <Box p={2}>
-            <Link to='/gre-words'>GRE Volcabulary Flashcards</Link>
-          </Box>
-          <Box p={2}>
-            <Link to='/me'>About Me</Link>
-          </Box>
+          <Grid gap={2} columns={[5, 3]}>
+            <Flex p={3}
+            sx={{
+              alignItems: "center"
+            }}>
+              <Avatar m={2} src={chefHatIcon} sx={{ width: "30px" }} />
+              <Link to='/cookings'>
+                <Text variant="icon">My Cooking Recipes</Text>
+              </Link>
+            </Flex>
+            <Flex p={3}
+            sx={{
+              alignItems: "center"
+            }}>
+              <Avatar m={2} src={gomokuIcon} sx={{ width: "30px" }} />
+              <Link to='/gomoku'><Text variant="icon">Gomoku(Connect-Five) Game</Text></Link>
+            </Flex>
+            <Flex p={3}
+            sx={{
+              alignItems: "center"
+            }}>
+              <Avatar m={2} src={conwayIcon} sx={{ width: "30px" }} />
+              <Link to='/game-of-life'><Text variant="icon">Conway's Game of Life Simulation</Text></Link>
+            </Flex>
+            <Flex p={3}
+            sx={{
+              alignItems: "center"
+            }}>
+              <Avatar m={2} src={greIcon} sx={{ width: "30px" }} />
+              <Link to='/gre-words'><Text variant="icon">GRE Volcabulary Flashcards</Text></Link>
+            </Flex>
+            <Flex p={3}
+            sx={{
+              alignItems: "center"
+            }}>
+              <Avatar m={2} src={meIcon} sx={{ width: "30px" }} />
+              <Link to='/me'>
+                <Text variant="icon">About Me</Text>
+              </Link>
+            </Flex>
+          </Grid>
         </Container>
       </ThemeProvider>
     )
