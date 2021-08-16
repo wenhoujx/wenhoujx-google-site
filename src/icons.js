e862dd8900b680a9957382874a57126a14c7acc5
Wenshuai Hou <whou@palantir.com>
2021-08-15 21:40:44 -0400

    Update: icons.js

---
+++ src/icons.js
@@ -0,0 +1,7 @@
+import chefHatIcon from "./images/chef-hat-50.png"
+import conwayIcon from  "./images/conway-icon.png"
+import gomokuIcon from  "./images/gomoku.png"
+import greIcon from  "./images/gre-icon.png"
+import meIcon from  "./images/me-icon.png"
+
+export {chefHatIcon, conwayIcon, gomokuIcon, greIcon, meIcon}
