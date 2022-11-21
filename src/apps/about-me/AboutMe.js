57f663f84c75c6bec3c5d1a7900e4ba640e447d5
Wenshuai Hou <wenhoujx@gmail.com>
2022-11-20 22:13:11 -0500

    update about me

--- src/apps/about-me/AboutMe.js
+++ src/apps/about-me/AboutMe.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Card from 'react-bootstrap/Card'
+import { Col, ListGroup, ListGroupItem, Row, Stack } from 'react-bootstrap'
 
 export function AboutMe() {
+    const skills = ['Java', 'JVM', "YourKit", 'undertow', 'jersey',
+        'Kubernete', 'AWS', 'EKS', 'clojure', 
+        'Python', 'Flask', 'Dagster', 'Airflow', 'Meltano', 'JupyterLab',
+        'Elasticsearch', 'Dynamo', 'SQL', 'snowflake', 'dbt',
+        'datomic', 'git', '(and more...)'
+    ]
     return (
         <Card className='w-80'>
             <Card.Body>
                 <Card.Title>I am Wenshuai Hou</Card.Title>
                 <Card.Text>
-                    I am a backend dev works on JAVA, JVM, python, kubernete, CI, jupyterlab, and tons of other things.
+                    I am a backend dev touched a lot of things:
+                    <Row md={5} >
+                        {skills.map(skill => (
+                            <Col>
+                                <div className="bg-light">{skill}</div>
+                            </Col>
+                        ))}
+                    </Row>
                 </Card.Text>
                 <Card.Text>
                     This personal website is my personal journey to explore the frontend and react.
